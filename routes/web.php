fa793399f40afd294c82696f9554f1180fd43875
Aleksandr Datsko <info@asdat.org>
2017-11-05 21:58:30 +0200

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-	return file_get_contents('https://u-vey-dev-ui.herokuapp.com/');
-});
-
-Route::get('/{url?}', function ($url) {
+Route::get('/{url?}', function ($url = '') {
 	echo $request = 'https://u-vey-dev-ui.herokuapp.com/'.$url;
     return file_get_contents($request);
 })->where('url', '(about|signin|signup|timeline|)');
 
 Route::prefix('api')->group(function () {
-    Route::get('{url?}', function ($url) {
+    Route::get('{url?}', function ($url = '') {
     	echo $request = 'https://u-vey-dev-api.herokuapp.com/api/'.$url;
         return file_get_contents($request);
     });
